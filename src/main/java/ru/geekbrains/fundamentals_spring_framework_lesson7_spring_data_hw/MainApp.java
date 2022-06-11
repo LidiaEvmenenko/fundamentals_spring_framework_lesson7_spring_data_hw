985e01c66976ea7e8ae2393c4d1ebd91daeb723a
LidiaEvmenenko <inu-chan@mail.ru>
2022-06-11 16:55:02 +0500

    Initial commit

---
+++ src/main/java/ru/geekbrains/fundamentals_spring_framework_lesson7_spring_data_hw/MainApp.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.fundamentals_spring_framework_lesson7_spring_data_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/fundamentals_spring_framework_lesson7_spring_data_hw/FundamentalsSpringFrameworkLesson7SpringDataHwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.fundamentals_spring_framework_lesson7_spring_data_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsSpringFrameworkLesson7SpringDataHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
